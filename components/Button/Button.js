d148a6ce0026a9f956bd168ace9746905deb947d
Tran Minh Nhut <huanhiendanh@gmail.com>
2020-05-14 22:14:23 +0700

    init

---
+++ components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ButtonWrapper, ButtonMain } from './Button.style';
+
+const Button = ({}) => {
+    return <ButtonWrapper>
+        <ButtonMain>cac</ButtonMain>
+    </ButtonWrapper>
+}
+
+export default Button;
---
+++ components/Button/Button.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ButtonWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+export const ButtonMain = styled.button`
+`

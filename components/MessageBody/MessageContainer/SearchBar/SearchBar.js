8b57109e44ea3102e2752993bc9177b3b0c44d1c
Tran Minh Nhut <nhuttm@dgroup.co>
2020-05-30 23:59:03 +0700

    create connection

--- components/MessageBody/MessageContainer/SearchBar/SearchBar.js
+++ components/MessageBody/MessageContainer/SearchBar/SearchBar.js
@@ -2,13 +2,36 @@ import React from 'react';
 import { Formik, Form } from 'formik';
 import TextInput from 'components/Form/TextInput';
 import Button from 'components/Button';
+import { commitMutation } from 'react-relay';
+import environment from 'relay/RelayEnvironment';
+import { CreateConnection } from 'relay/graphql/RoomGraph';
 
 const SearchBar = ({}) => {
     return <Formik
         initialValues={{
             email: ''
         }}
-        onSubmit={() => console.log('Hello')}
+        onSubmit={({ email }) => {
+            console.log(email);
+            commitMutation(environment(), {
+                mutation: CreateConnection,
+                variables: {
+                  email,
+                },
+                onCompleted: ({ CreateConnection }, errors) => {
+                  if (errors) {
+                    console.log(errors);
+                  }
+                  else {
+                    console.log('success');
+                  }
+                  
+                },
+                onError: (err) => {
+                  console.log(err);
+                },
+              });
+        }}
     >   
         <Form>
             <TextInput 
